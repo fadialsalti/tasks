5c6800e339dcd7801997917ed1cb0874d6fa28aa
fadialsalti <eng_fadi77@hotmail.com>
2023-06-20 22:47:46 +0200

    add footer and some css

---
+++ services/tasks_ui/src/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+    <footer class="footer">
+        <div class="footer-content">
+            <p>&copy; 2023 Company. All rights reserved.</p>
+            <p>Contact: eng_fadi77@hotmail.com</p>
+        </div>
+    </footer>
+</template>
+  
+<style scoped>
+.footer {
+  margin-top: 40px;
+  padding: 20px;
+  background-color: #f0f2f5;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  color: #333;
+}
+
+.footer p {
+  margin: 0;
+}
+
+.footer a {
+  color: #333;
+  text-decoration: none;
+}
+
+.footer a:hover {
+  text-decoration: underline;
+}
+
+.footer .social-icons {
+  margin-top: 10px;
+}
+
+.footer .social-icons a {
+  margin-right: 10px;
+}
+
+.footer .social-icons a:last-child {
+  margin-right: 0;
+}
+
+.footer .social-icons i {
+  font-size: 24px;
+}
+
+.footer .social-icons i:hover {
+  color: #555;
+}
+</style>
+
+  
